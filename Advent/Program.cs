22bb3d0574a48fa09c2724916c3abae81a6d8c96
Adams, Bracken <ah3353@Halff.com>
2020-12-07 09:45:25 -0600

    Day 6 challenge 2

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -7,33 +7,125 @@ namespace Advent
     class Program
     {
         /// <summary>
-        
+        ///--- Day 6: Custom Customs ---
+        ///As your flight approaches the regional airport where you'll switch to a much larger plane, customs declaration forms are distributed to the passengers.
+        ///
+        ///The form asks a series of 26 yes-or-no questions marked a through z.All you need to do is identify the questions for which anyone in your group answers "yes". Since your group is just you, this doesn't take very long.
+        ///
+        ///However, the person sitting next to you seems to be experiencing a language barrier and asks if you can help.For each of the people in their group, you write down the questions for which they answer "yes", one per line.For example:
+        ///
+        ///abcx
+        ///abcy
+        ///abcz
+        ///In this group, there are 6 questions to which anyone answered "yes": a, b, c, x, y, and z. (Duplicate answers to the same question don't count extra; each question counts at most once.)
+        ///
+        ///Another group asks for your help, then another, and eventually you've collected answers from every group on the plane (your puzzle input). Each group's answers are separated by a blank line, and within each group, each person's answers are on a single line. For example:
+        ///
+        ///abc
+        ///
+        ///a
+        ///b
+        ///c
+        ///
+        ///ab
+        ///ac
+        ///
+        ///a
+        ///a
+        ///a
+        ///a
+        ///
+        ///b
+        ///This list represents answers from five groups:
+        ///
+        ///The first group contains one person who answered "yes" to 3 questions: a, b, and c.
+        ///The second group contains three people; combined, they answered "yes" to 3 questions: a, b, and c.
+        ///The third group contains two people; combined, they answered "yes" to 3 questions: a, b, and c.
+        ///The fourth group contains four people; combined, they answered "yes" to only 1 question, a.
+        ///The last group contains one person who answered "yes" to only 1 question, b.
+        ///In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
+        ///
+        ///For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
+        ///
+        ///Your puzzle answer was 6530.
+        ///
+        ///The first half of this puzzle is complete! It provides one gold star: *
+        ///--- Part Two ---
+        ///As you finish the last group's customs declaration, you notice that you misread one word in the instructions:
+        ///
+        ///You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!
+        ///
+        ///Using the same example as above:
+        ///
+        ///abc
+        ///
+        ///a
+        ///b
+        ///c
+        ///
+        ///ab
+        ///ac
+        ///
+        ///a
+        ///a
+        ///a
+        ///a
+        ///
+        ///b
+        ///This list represents answers from five groups:
+        ///
+        ///In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
+        ///In the second group, there is no question to which everyone answered "yes".
+        ///In the third group, everyone answered yes to only 1 question, a.Since some people did not answer "yes" to b or c, they don't count.
+        ///In the fourth group, everyone answered yes to only 1 question, a.
+        ///In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+        ///In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+        ///
+        ///For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
         /// </summary>
-        public static List<List<char>> input = new List<List<char>>();
+        public static List<string> input = new List<string>();
 
         public static void Main(string[] args)
         {
             ReadInput();
 
-            Console.WriteLine("Total count: " + input.Sum(x => x.Count));
+            Console.WriteLine("Total count: " + input.Sum(x => x.Length));
         }
 
         private static void ReadInput()
         {
             string[] lines = System.IO.File.ReadAllLines(@"C:/Users/ah3353/source/repos/Advent/Advent/Inputs/Input.txt");
 
-            var newString = "";
+            var groupAnswers = new List<string>();
             foreach (var line in lines)
             {
                 if (line != "")
-                    newString += line;
+                    groupAnswers.Add(line);
                 else
                 {
-                    input.Add(newString.Distinct().ToList());
-                    newString = "";
+                    input.Add(GetReoccurringCharacters(groupAnswers));
+                    groupAnswers = new List<string>();
                 }
             }
-            input.Add(newString.Distinct().ToList());
+            input.Add(GetReoccurringCharacters(groupAnswers));
+        }
+
+        private static string GetReoccurringCharacters(List<string> groupAnswers)
+        {
+            var finalGroupAnswer = "";
+            foreach (var character in groupAnswers[0])
+            {
+                var includeCharacter = true;
+                for (int i = 1; i < groupAnswers.Count; i++)
+                {
+                    if (!groupAnswers[i].Contains(character))
+                        includeCharacter = false;
+                }
+                if (includeCharacter)
+                    finalGroupAnswer += character;
+            }
+
+            return finalGroupAnswer;
         }
     }
 }
