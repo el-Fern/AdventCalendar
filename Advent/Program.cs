7e9dce15506a949fcfe1e2063c420c0e278966d5
Adams, Bracken <ah3353@Halff.com>
2020-12-07 11:00:37 -0600

    Day 7 challenge 2

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -39,6 +39,31 @@ namespace Advent
         ///So, in this example, the number of bag colors that can eventually contain at least one shiny gold bag is 4.
         ///
         ///How many bag colors can eventually contain at least one shiny gold bag? (The list of rules is quite long; make sure you get all of it.)
+        ///--- Part Two ---
+        ///It's getting pretty expensive to fly these days - not because of ticket prices, but because of the ridiculous number of bags you need to buy!
+        ///
+        ///Consider again your shiny gold bag and the rules from the above example:
+        ///
+        ///faded blue bags contain 0 other bags.
+        ///dotted black bags contain 0 other bags.
+        ///vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+        ///dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+        ///So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2 vibrant plum bags(and the 11 bags within each of those) : 1 + 1*7 + 2 + 2*11 = 32 bags!
+        ///
+        ///Of course, the actual rules have a small chance of going several levels deeper than this example; be sure to count all of the bags, even if the nesting becomes topologically impractical!
+        ///
+        ///Here's another example:
+        ///
+        ///shiny gold bags contain 2 dark red bags.
+        ///dark red bags contain 2 dark orange bags.
+        ///dark orange bags contain 2 dark yellow bags.
+        ///dark yellow bags contain 2 dark green bags.
+        ///dark green bags contain 2 dark blue bags.
+        ///dark blue bags contain 2 dark violet bags.
+        ///dark violet bags contain no other bags.
+        ///In this example, a single shiny gold bag must contain 126 other bags.
+        ///
+        ///How many individual bags are required inside your single shiny gold bag?
         /// </summary>
         public static List<Rule> input = new List<Rule>();
 
@@ -46,44 +71,18 @@ namespace Advent
         {
             ReadInput();
 
-            Console.WriteLine("Bags that can contain a shiny gold bag: " + BagsThatCanContainShinyGold());
+            Console.WriteLine("Bags that are inside a shiny gold bag: " + TotalBagsInside("shiny gold"));
         }
 
-        private static int BagsThatCanContainShinyGold()
+        private static int TotalBagsInside(string bag)
         {
-            var countThatCanContain = 0;
-            var possibleBags = input.Select(x => x.Bag).ToList();
-
-            foreach (var bag in possibleBags)
-            {
-                if (CanContainShinyGold(bag, new List<string>()))
-                    countThatCanContain++;
-            }
-
-            return countThatCanContain;
-        }
-
-        private static bool CanContainShinyGold(string bag, List<string> checkedBags)
-        {
-            var canContain = false;
-            var ruleForBag = input.First(x=>x.Bag == bag);
-            checkedBags.Add(bag);
-
+            var ruleForBag = input.First(x => x.Bag == bag);
+            var bagsContained = 0;
             foreach (var containRule in ruleForBag.ContainedBags)
             {
-                if (!checkedBags.Contains(containRule.Bag))
-                {
-                    if (containRule.Bag == "shiny gold")
-                        return true;
-                    else
-                    {
-                        if (CanContainShinyGold(containRule.Bag, checkedBags))
-                            canContain = true;
-                    }
-                }
+                bagsContained += (containRule.Count * TotalBagsInside(containRule.Bag)) + containRule.Count;
             }
-
-            return canContain;
+            return bagsContained;
         }
 
         private static void ReadInput()
