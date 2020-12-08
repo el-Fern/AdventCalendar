b750e97cdae9977842c68dc0df42eddeb4c53677
Adams, Bracken <ah3353@Halff.com>
2020-12-08 07:55:57 -0600

    Day 8 challenge2

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -49,6 +49,40 @@ namespace Advent
         ///Immediately before the program would run an instruction a second time, the value in the accumulator is 5.
         ///
         ///Run your copy of the boot code.Immediately before any instruction is executed a second time, what value is in the accumulator?
+        ///--- Part Two ---
+        ///After some careful analysis, you believe that exactly one instruction is corrupted.
+        ///
+        ///Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed to be a jmp. (No acc instructions were harmed in the corruption of this boot code.)
+        ///
+        ///The program is supposed to terminate by attempting to execute an instruction immediately after the last instruction in the file. By changing exactly one jmp or nop, you can repair the boot code and make it terminate correctly.
+        ///
+        ///For example, consider the same program from above:
+        ///
+        ///nop +0
+        ///acc +1
+        ///jmp +4
+        ///acc +3
+        ///jmp -3
+        ///acc -99
+        ///acc +1
+        ///jmp -4
+        ///acc +6
+        ///If you change the first instruction from nop +0 to jmp +0, it would create a single-instruction infinite loop, never leaving that instruction. If you change almost any of the jmp instructions, the program will still eventually find another jmp instruction and loop forever.
+        ///
+        ///However, if you change the second-to-last instruction (from jmp -4 to nop -4), the program terminates! The instructions are visited in this order:
+        ///
+        ///nop +0  | 1
+        ///acc +1  | 2
+        ///jmp +4  | 3
+        ///acc +3  |
+        ///jmp -3  |
+        ///acc -99 |
+        ///acc +1  | 4
+        ///nop -4  | 5
+        ///acc +6  | 6
+        ///After the last instruction(acc +6), the program terminates by attempting to run the instruction below the last instruction in the file.With this change, after the program terminates, the accumulator contains the value 8 (acc +1, acc +1, acc +6).
+        ///
+        ///Fix the program so that it terminates normally by changing exactly one jmp(to nop) or nop(to jmp). What is the value of the accumulator after the program terminates?
         /// </summary>
         public static List<string> input = new List<string>();
 
@@ -59,34 +93,55 @@ namespace Advent
             var accumulator = 0;
             var visitedIndexes = new List<int>();
 
-            for (int i = 0; i < input.Count; i++)
+            for (int x = 0; x < input.Count; x++)
             {
-                if (visitedIndexes.Contains(i))
+                if (input[x].StartsWith("jmp"))
+                    input[x] = "nop" + input[x].Substring(3, input[x].Length - 3);
+                else if (input[x].StartsWith("nop"))
+                    input[x] = "jmp" + input[x].Substring(3, input[x].Length - 3);
+                else
+                    continue;
+
+                accumulator = 0;
+                visitedIndexes = new List<int>();
+
+                for (int i = 0; i < input.Count; i++)
                 {
-                    break;
+                    if (visitedIndexes.Contains(i))
+                    {
+                        break;
+                    }
+
+                    visitedIndexes.Add(i);
+
+                    var action = input[i].Substring(0, 3);
+                    var amount = input[i].Substring(4, input[i].Length - 4);
+                    if (action == "acc")
+                    {
+                        if (amount.Substring(0, 1) == "+")
+                            accumulator += Convert.ToInt32(amount.Substring(1, amount.Length - 1));
+                        else
+                            accumulator -= Convert.ToInt32(amount.Substring(1, amount.Length - 1));
+                    }
+                    else if (action == "jmp")
+                    {
+                        if (amount.Substring(0, 1) == "+")
+                            i += (Convert.ToInt32(amount.Substring(1, amount.Length - 1)) - 1);
+                        else
+                            i -= (Convert.ToInt32(amount.Substring(1, amount.Length - 1)) + 1);
+                    }
+                    if (i == input.Count - 1)
+                    {
+                        Console.WriteLine("Accumulator: " + accumulator);
+                    }
                 }
 
-                visitedIndexes.Add(i);
-
-                var action = input[i].Substring(0, 3);
-                var amount = input[i].Substring(4, input[i].Length - 4);
-                if (action == "acc")
-                {
-                    if (amount.Substring(0, 1) == "+")
-                        accumulator += Convert.ToInt32(amount.Substring(1, amount.Length - 1));
-                    else
-                        accumulator -= Convert.ToInt32(amount.Substring(1, amount.Length - 1));
-                }
-                else if (action == "jmp")
-                {
-                    if (amount.Substring(0, 1) == "+")
-                        i += (Convert.ToInt32(amount.Substring(1, amount.Length - 1)) - 1);
-                    else
-                        i -= (Convert.ToInt32(amount.Substring(1, amount.Length - 1)) + 1);
-                }
+                if (input[x].StartsWith("jmp"))
+                    input[x] = "nop" + input[x].Substring(3, input[x].Length - 3);
+                else if (input[x].StartsWith("nop"))
+                    input[x] = "jmp" + input[x].Substring(3, input[x].Length - 3);
             }
 
-            Console.WriteLine("Accumulator: " + accumulator);
         }
 
         private static void ReadInput()
