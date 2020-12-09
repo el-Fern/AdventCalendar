9fc07ff0ba7ea32e19a498765cf5e995543a98f8
Adams, Bracken <ah3353@Halff.com>
2020-12-09 09:50:43 -0600

    Day 9 challenge 2

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -52,6 +52,36 @@ namespace Advent
         ///In this example, after the 5-number preamble, almost every number is the sum of two of the previous 5 numbers; the only number that does not follow this rule is 127.
         ///
         ///The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers before it. What is the first number that does not have this property?
+        ///--- Part Two ---
+        ///The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
+        ///
+        ///Again consider the above example:
+        ///
+        ///35
+        ///20
+        ///15
+        ///25
+        ///47
+        ///40
+        ///62
+        ///55
+        ///65
+        ///95
+        ///102
+        ///117
+        ///150
+        ///182
+        ///127
+        ///219
+        ///299
+        ///277
+        ///309
+        ///576
+        ///In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+        ///
+        ///To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
+        ///
+        ///What is the encryption weakness in your XMAS-encrypted list of numbers?
         /// </summary>
         public static List<double> input = new List<double>();
 
@@ -59,7 +89,11 @@ namespace Advent
         {
             ReadInput();
 
-            Console.WriteLine("First number without the numbers before adding to it: " + GetWrongNumber());
+            var wrongNumber = GetWrongNumber();
+
+            var encryptionWeakness = GetEncryptionWeakness(wrongNumber);
+
+            Console.WriteLine("Encryption weakness is: " + encryptionWeakness);
         }
 
         private static double GetWrongNumber()
@@ -82,6 +116,24 @@ namespace Advent
                 }
                 if (!foundMatch)
                     return numberToCheck;
+            }
+            return 0;
+        }
+
+        private static double GetEncryptionWeakness(double wrongNumber)
+        {
+            for (int i = 0; i < input.Count; i++)
+            {
+                var contiguousNumbers = new List<double>();
+                contiguousNumbers.Add(input[i]);
+                for(int x = i+1; x<input.Count;x++)
+                {
+                    contiguousNumbers.Add(input[x]);
+                    if (contiguousNumbers.Sum() > wrongNumber)
+                        continue;
+                    else if (contiguousNumbers.Sum() == wrongNumber)
+                        return contiguousNumbers.Max() + contiguousNumbers.Min();
+                }
             }
             return 0;
         }
