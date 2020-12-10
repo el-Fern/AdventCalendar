d977c5a29a9c82e594b1403ad4eef48cdbd41ce7
Adams, Bracken <ah3353@Halff.com>
2020-12-10 07:39:38 -0600

    Day 10 challenge 1

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Advent
 {
@@ -8,17 +9,49 @@ namespace Advent
         /// <summary>
         
         /// </summary>
-        public static List<string> input = new List<string>();
+        public static List<int> input = new List<int>();
 
         public static void Main(string[] args)
         {
             ReadInput();
 
+            Console.WriteLine("Jolt difference product: " + FindJoltageDifferences());
+        }
+
+        private static int FindJoltageDifferences()
+        {
+            var joltage1Difference = 0;
+            var joltage3Difference = 0;
+            for (int i = 0; i < input.Count; i++)
+            {
+                if(i == 0)
+                {
+                    if(input[i] == 1)
+                        joltage1Difference++;
+                    else if (input[i] == 3)
+                        joltage3Difference++;
+                }    
+                else if (input[i] - input[i - 1] == 1)
+                    joltage1Difference++;
+                else if (input[i] - input[i - 1] == 3)
+                    joltage3Difference++;
+                else
+                    Console.WriteLine("Unexpected joltage difference: " + input[i].ToString() + input[i - 1].ToString());
+            }
+
+            //the device's built-in adapter is always 3 higher than the highest adapter
+            joltage3Difference++;
+
+            return joltage1Difference * joltage3Difference;
         }
 
         private static void ReadInput()
         {
-            input.AddRange(System.IO.File.ReadAllLines(@"C:/Users/ah3353/source/repos/Advent/Advent/Inputs/Input.txt"));
+            input.AddRange(System.IO.File.ReadAllLines(@"C:/Users/ah3353/source/repos/Advent/Advent/Inputs/Input.txt")
+                .Select(x=>Convert.ToInt32(x))
+                .OrderBy(x=>x)
+                .ToList());
         }
+
     }
 }
